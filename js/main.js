b645bc0a9e556b97d736e4cf22cc03b1b529f8f5
Ibrahim Kolabalogun <115741084+bolajeee@users.noreply.github.com>
2023-03-17 20:39:52 +0100

    update

--- js/main.js
+++ js/main.js
@@ -30,64 +30,64 @@
     
 
     
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
+    // // Smooth scrolling on the navbar links
+    // $(".navbar-nav a").on('click', function (event) {
+    //     if (this.hash !== "") {
+    //         event.preventDefault();
             
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 50
-            }, 1500, 'easeInOutExpo');
+    //         $('html, body').animate({
+    //             scrollTop: $(this.hash).offset().top - 50
+    //         }, 1500, 'easeInOutExpo');
             
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
+    //         if ($(this).parents('.navbar-nav').length) {
+    //             $('.navbar-nav .active').removeClass('active');
+    //             $(this).closest('a').addClass('active');
+    //         }
+    //     }
+    // });
     
     
-    // Screenshot carousel
-    $(".screenshot-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            }
-        }
-    });
+//     // Screenshot carousel
+//     $(".screenshot-carousel").owlCarousel({
+//         autoplay: true,
+//         dots: true,
+//         loop: true,
+//         responsive: {
+//             0:{
+//                 items:1
+//             },
+//             576:{
+//                 items:2
+//             },
+//             768:{
+//                 items:3
+//             },
+//             992:{
+//                 items:4
+//             }
+//         }
+//     });
 
 
-    // Testimonials carousel
-    $(".testimonials-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-})(jQuery);
+//     // Testimonials carousel
+//     $(".testimonials-carousel").owlCarousel({
+//         autoplay: true,
+//         dots: true,
+//         loop: true,
+//         responsive: {
+//             0:{
+//                 items:1
+//             },
+//             576:{
+//                 items:2
+//             },
+//             768:{
+//                 items:2
+//             },
+//             992:{
+//                 items:3
+//             }
+//         }
+//     });
+// })(jQuery);
 
